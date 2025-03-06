20ae0181eff76b2c0dc5bbde4941432239ab5d81
Lukas Czaplinski <lukasz@wooting.io>
2025-03-06 11:25:35 +0100

    format; add merlin

--- flake.nix
+++ flake.nix
@@ -13,21 +13,15 @@
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
-      imports = [
-        inputs.process-compose-flake.flakeModule
-      ];
+      imports = [ inputs.process-compose-flake.flakeModule ];
       perSystem = { self', pkgs, config, lib, ... }: {
         # `process-compose.foo` will add a flake package output called "foo".
         # Therefore, this will add a default package that you can build using
         # `nix build` and run using `nix run`.
         process-compose."default" = { config, ... }:
-          let
-            dbName = "sample";
-          in
-          {
-            imports = [
-              inputs.services-flake.processComposeModules.default
-            ];
+          let dbName = "sample";
+          in {
+            imports = [ inputs.services-flake.processComposeModules.default ];
 
             # services.postgres."pg1" = {
             #   enable = true;
@@ -52,25 +46,24 @@
               command = pkgs.writeShellApplication {
                 name = "bun-run-watch";
                 text = ''
-                    bun run watch
+                  bun run watch
                 '';
               };
             };
             settings.processes.serve = {
-                          command = pkgs.writeShellApplication {
-                            name = "bun-run-serve";
-                            text = ''
-                                bun run serve
-                            '';
-                          };
-                        };
+              command = pkgs.writeShellApplication {
+                name = "bun-run-serve";
+                text = ''
+                  bun run serve
+                '';
+              };
+            };
           };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = [
-            config.process-compose."default".services.outputs.devShell
-          ];
-          nativeBuildInputs = [ pkgs.just pkgs.bun pkgs.opam];
+          inputsFrom =
+            [ config.process-compose."default".services.outputs.devShell ];
+          nativeBuildInputs = [ pkgs.just pkgs.bun pkgs.opam ];
         };
       };
     };
